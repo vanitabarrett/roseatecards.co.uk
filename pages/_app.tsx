ae3dd6906a8c021b58cfb7a3ac5481dfc621630a
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-11-10 20:52:00 +0000

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,21 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Roseate Cards</title>
+
+        <meta charSet="utf-8" ></meta>
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="roseate cards, cards, greeting, stationery, envelopes, christmas, birthday" />
+        <meta name="description" content="Greeting cards and stationery. Send a little love in the post ❤️" />
+
+        <meta property="og:title" content="Roseate Cards" />
+        <meta property="og:image" content="/social/social.png" />
+        <meta property="og:description" content="Greeting cards and stationery. Send a little love in the post ❤️" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Roseate Cards" />
+        <meta property="twitter:description" content="Greeting cards and stationery. Send a little love in the post ❤️" />
+        <meta property="twitter:image" content="/social/social.png" />
+
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
       </Head>
       <div className="page-container">
