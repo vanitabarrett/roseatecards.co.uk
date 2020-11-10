87f100bc0e7bda7988d82b39d284b421807a8e4f
andymsuk <contact@andymsmith.co.uk>
2020-11-10 20:41:40 +0000

    Fix classnames

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="gel-wrap home home-page">
+    <div className="gel-wrap home-page">
       <Link href="/cards/christmas">
-        <a className="home__featured">
-          <p className="home__featured-text-wrapper">
-            <span className="home__featured-text">It's beginning to look a lot like</span><br/><span className="home__featured-text home__featured-large-text">Christmas</span>
+        <a className="home-page__featured">
+          <p className="home-page__featured-text-wrapper">
+            <span className="home-page__featured-text">It's beginning to look a lot like</span><br/><span className="home-page__featured-text home-page__featured-large-text">Christmas</span>
           </p>
         </a>
       </Link>
 
-      <div className="home__covid-banner"><strong>COVID-19 update:</strong> we're still open;&nbsp;
+      <div className="home-page__covid-banner"><strong>COVID-19 update:</strong> we're still open;&nbsp;
         <Link href="/contact#covid19">
           <a>shipping may be delayed</a>
         </Link>
--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -2,16 +2,16 @@ import Head from 'next/head';
 
 export default function PrivacyPolicy() {
   return (
-    <div className="gel-wrap privacy-policy-page privacy-policy gel-2/3">
+    <div className="gel-wrap privacy-policy-page gel-2/3">
       <Head>
         <title>Privacy Policy - Roseate Cards</title>
       </Head>
       <h1 className="page-title">Privacy Policy</h1>
       <p>Roseate Cards operates the website <a href="/">www.roseatecards.co.uk</a>. Roseate Cards is the data controller for any page on the roseatecards.co.uk domain. If you follow a link to a third-party site, for example: Etsy, that organisation is the data controller and is responsible for processing any data you share with them, unless stated otherwise.</p>
 
-      <h2 className="privacy-policy__heading">What data we collect</h2>
+      <h2 className="privacy-policy-page__heading">What data we collect</h2>
       <p>We do not collect any data from you on www.roseatecards.co.uk. If you choose to purchase a product from us, you will be directed to Etsy <a href="https://www.etsy.com/uk/shop/roseatecards">(https://www.etsy.com/uk/shop/roseatecards)</a>. Any purchase made via the Roseate Cards Etsy shop will send the following data to us:</p>
-      <ul className="privacy-policy__list">
+      <ul className="privacy-policy-page__list">
         <li>name and postal address</li>
         <li>email address</li>
         <li>personalisation information (where applicable)</li>
@@ -20,7 +20,7 @@ export default function PrivacyPolicy() {
 
       <p>Details of your purchase will be stored within Etsy and only provided to our third-party printers in order to fulfil your order. At no point are they passed on to any other third-parties.</p>
       <p>We will not:</p>
-      <ul className="privacy-policy__list">
+      <ul className="privacy-policy-page__list">
         <li>sell or rent your data to third parties</li>
         <li>share your data with third parties for marketing purposes</li>
         <li>use your data in analytics</li>
@@ -28,24 +28,24 @@ export default function PrivacyPolicy() {
 
       <p>We will share your data if we are required to do so by law - for example, by court order, or to prevent fraud or other crime.</p>
 
-      <h2 className="privacy-policy__heading">Why we need your data</h2>
+      <h2 className="privacy-policy-page__heading">Why we need your data</h2>
       <p>We only collect the data you submit when purchasing a product via Roseate Cards. This information is required in order to fulfil your order and is not used for any other purpose. This data may be anonymised and analysed by us to gain insights into our sales.</p>
 
-      <h2 className="privacy-policy__heading">How long we keep your data</h2>
+      <h2 className="privacy-policy-page__heading">How long we keep your data</h2>
       <p>We will only retain your personal data for as long as it is needed for the purposes set out in this document and as long as the law requires us to.</p>
 
-      <h2 className="privacy-policy__heading">Children’s privacy protection</h2>
+      <h2 className="privacy-policy-page__heading">Children’s privacy protection</h2>
       <p>Our services are not designed for, or intentionally targeted at, children 13 years of age or younger. We do not intentionally collect or maintain data about anyone under the age of 13.</p>
 
-      <h2 className="privacy-policy__heading">Your rights</h2>
+      <h2 className="privacy-policy-page__heading">Your rights</h2>
       <p>You have the right to request:</p>
-      <ul className="privacy-policy__list">
+      <ul className="privacy-policy-page__list">
         <li>information about how your personal data is processed</li>
         <li>a copy of that personal data</li>
       </ul>
 
       <p>You can also:</p>
-      <ul className="privacy-policy__list">
+      <ul className="privacy-policy-page__list">
         <li>raise an objection about how your personal data is processed</li>
         <li>request that your personal data is erased if there is no longer a justification for it</li>
         <li>ask that the processing of your personal data is restricted in certain circumstances</li>
@@ -53,24 +53,24 @@ export default function PrivacyPolicy() {
 
       <p>Please get in touch via <a href="mailto:support@roseatecards.co.uk">support@roseatecards.co.uk</a></p>
 
-      <h2 className="privacy-policy__heading">Links to other websites</h2>
+      <h2 className="privacy-policy-page__heading">Links to other websites</h2>
       <p>Roseate Cards may contain links to other websites. This privacy notice only applies to Roseate Cards, and does not cover other websites that we link to.</p>
 
       <p>If you go to another website from this one, read the privacy policy on that website to find out what it does with your information.</p>
 
-      <h2 className="privacy-policy__heading">Contact us</h2>
+      <h2 className="privacy-policy-page__heading">Contact us</h2>
       <p>Contact us if you:</p>
-      <ul className="privacy-policy__list">
+      <ul className="privacy-policy-page__list">
         <li>have a question about anything in this privacy notice</li>
         <li>think that your personal data has been misused or mishandled</li>
       </ul>
 
       <p>Email us: <a href="mailto:support@roseatecards.co.uk">support@roseatecards.co.uk</a></p>
 
-      <h2 className="privacy-policy__heading">Changes to this policy</h2>
+      <h2 className="privacy-policy-page__heading">Changes to this policy</h2>
       <p>We may change this privacy policy. In that case, the ‘last updated’ date at the bottom of this page will also change. Any changes to this privacy policy will apply to you and your data immediately.</p>
 
-      <p className="privacy-policy__updated">Last updated: 7 November 2020</p>
+      <p className="privacy-policy-page__updated">Last updated: 7 November 2020</p>
     </div>
   );
 }
