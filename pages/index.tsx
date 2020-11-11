a4352e63893936bbcbe598cdbdd5d4b1d4aefadf
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-11-11 14:32:29 +0000

    Add info section to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="gel-wrap home-page">
+
       <Link href="/cards/christmas">
         <a className="home-page__featured">
           <p className="home-page__featured-text-wrapper">
@@ -16,6 +17,22 @@ export default function Home() {
           <a>shipping may be delayed</a>
         </Link>
       </div>
+
+      <div className="home-page__info gel-layout">
+        <div className="home-page__info__section gel-layout__item gel-1/3">
+          <img className="home-page__info__image" src="/svgs/sparkles.svg" alt="" />
+          <p className="home-page__info__text">Unique card designs that you won't find anywhere else</p>
+        </div>
+        <div className="home-page__info__section gel-layout__item gel-1/3">
+          <img className="home-page__info__image" src="/svgs/email-dark.svg" alt="" />
+          <p className="home-page__info__text">Direct to recipient shipping available to any UK address</p>
+        </div>
+        <div className="home-page__info__section gel-layout__item gel-1/3">
+          <img className="home-page__info__image" src="/svgs/sustainability.svg" alt=""/>
+          <p className="home-page__info__text">Committed to high quality and sustainable products</p>
+        </div>
+      </div>
+
     </div>
   );
 }
