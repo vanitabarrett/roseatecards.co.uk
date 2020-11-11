d0aa1558f7daffd0331b2f479e3edb4760ec9305
andymsuk <contact@andymsmith.co.uk>
2020-11-11 20:09:31 +0000

    Sort products by featured then last modified

--- lib/shopData.ts
+++ lib/shopData.ts
@@ -1,5 +1,6 @@
 import shopData from '../shop-data.json';
-import { Category, CategoryNoProducts } from '../types';
+import { Category, CategoryNoProducts, Product } from '../types';
+import { sortByKey } from './arrays';
 
 export function getCategoryInfoNoProducts(categoryId): CategoryNoProducts {
   const { subCategories, ...categoryInfo } = getCategoryInfo(categoryId);
@@ -11,4 +12,23 @@ export function getCategoryInfoNoProducts(categoryId): CategoryNoProducts {
 
 export function getCategoryInfo(categoryId): Category {
   return shopData.find(({ id }) => id === categoryId);
+}
+
+export function sortProducts(products: Product[]): Product[] {
+  const { featuredProducts, nonFeaturedProducts } = products.reduce((accumulator, product) => {
+    if (product.featuredRank) {
+      return {
+        featuredProducts: [...accumulator.featuredProducts, product],
+        nonFeaturedProducts: accumulator.nonFeaturedProducts
+      }
+    }
+    return {
+      featuredProducts: accumulator.featuredProducts,
+      nonFeaturedProducts: [...accumulator.nonFeaturedProducts, product]
+    }
+  }, { featuredProducts: [], nonFeaturedProducts: [] });
+  return [
+    ...sortByKey(featuredProducts, 'featuredRank'),
+    ...sortByKey(nonFeaturedProducts, 'lastModified').reverse()
+  ];
 }
