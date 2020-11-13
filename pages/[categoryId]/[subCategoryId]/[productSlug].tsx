b76402f9a32b4a67a1b01832065755497d6940ae
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-11-13 19:17:49 +0000

    Remove unused import

--- pages/[categoryId]/[subCategoryId]/[productSlug].tsx
+++ pages/[categoryId]/[subCategoryId]/[productSlug].tsx
@@ -5,7 +5,6 @@ import htmlParser from 'html-react-parser';
 
 import shopData from '../../../shop-data.json';
 import { getCategoryInfo, getCategoryInfoNoProducts } from '../../../lib/shopData';
-import { NavLink } from '../../../types';
 import Breadcrumbs from '../../../components/Breadcrumbs';
 import ImageCarousel from '../../../components/ImageCarousel';
 
