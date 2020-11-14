cf77345c7d8adbaa4cb1b1ad42cf951fa92230f2
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-11-14 16:16:08 +0000

    Fix aria-hidden on whole header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import Nav from './Nav';
 
 export default function Header() {
   return (
-    <header className="header" aria-hidden="true">
+    <header className="header">
       <Link href="/">
-        <a className="header__link" tabIndex={-1}>
+        <a className="header__link" tabIndex={-1} aria-hidden="true">
           <img src="/svgs/logo.svg" alt="Roseate Cards" className="header__logo" width="250px" />
         </a>
       </Link>
