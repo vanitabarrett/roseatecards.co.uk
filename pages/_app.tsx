4933bfe9831940aeb7becc5d901969452033576e
andymsuk <contact@andymsmith.co.uk>
2020-11-14 17:18:01 +0000

    Improve screenreader experience with client-side-routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,34 @@ import Header from "../components/Header";
 import Footer from '../components/Footer';
 
 import '../styles/app.scss';
+import { useCallback, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
+  const skipLink = useRef<HTMLAnchorElement>();
+  const pageReadOut = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    skipLink.current.classList.add('screenreader-only');
+    pageReadOut.current.innerText = window.document.title;
+    skipLink.current.focus();
+  }, [asPath]);
+
+  const onSkipBlur = useCallback(() => {
+    skipLink.current.classList.remove('screenreader-only');
+  }, [skipLink]);
+
   return (
     <>
       <Head>
         <title>Roseate Cards</title>
       </Head>
       <div className="page-container">
-        <a href="#main" className="skiplink">Skip to main page content</a>
+        <div ref={pageReadOut} className="screenreader-only" aria-live="polite"></div>
+        <a ref={skipLink} href="#main" onBlur={onSkipBlur} className="skiplink">Skip to main page content</a>
         <Header />
-        <main id="main" role="main" aria-live="assertive">
+        <main id="main" role="main">
           <Component {...pageProps} />
         </main>
       </div>
