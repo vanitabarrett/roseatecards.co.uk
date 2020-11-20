5f995fa4c1931cb6a5c2f30b1d7639a6072d350e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-11-20 14:44:58 +0000

    Add FAQ schema

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -6,10 +6,12 @@ interface Props {
 
 export default function FAQ({question, anchorId, children}: Props) {
   return (
-    <div className="faq" id={anchorId}>
-      <h2 className="faq__question">{question}</h2>
-      <div className="faq__answer">
-        {children}
+    <div className="faq" id={anchorId} itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
+      <h2 className="faq__question" itemProp="name">{question}</h2>
+      <div className="faq__answer" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
+        <div itemProp="text">
+          {children}
+        </div>
       </div>
     </div>
   )
