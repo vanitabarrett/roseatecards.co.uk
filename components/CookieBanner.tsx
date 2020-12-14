b562739213729a65ca630b3b31487e03303751bc
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-12-14 20:34:11 +0000

    Add cookie consent

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function CookieBanner() {
-  return (
+  const [showBanner, setShowBanner] = useState<boolean>(false);
+
+  function acceptCookies() {
+    document.cookie = 'cookie_preferences=1;expires=' + cookieExpiry() + ';domain=' + window.location.hostname + ';path=/';
+    setShowBanner(false);
+  }
+
+  function rejectCookies() {
+    document.cookie = 'cookie_preferences=0;expires=' + cookieExpiry() + ';domain=' + window.location.hostname + ';path=/';
+    setShowBanner(false);
+  }
+
+  useEffect(() => {
+    var cookiePreferencesSet = getCookie('cookie_preferences');
+    setShowBanner(!cookiePreferencesSet);
+  }, []);
+
+  return showBanner && (
     <div role='region' aria-label='cookie banner' className="cookie-banner">
       <div className="cookie-banner__wrapper">
         <div className="cookie-banner__text-wrapper">
@@ -8,10 +27,30 @@ export default function CookieBanner() {
         </div>
 
         <div className="cookie-banner__buttons">
-          <button className="cookie-banner__button">Accept analytics cookies</button>
-          <button className="cookie-banner__button">Reject analytics cookies</button>
+          <button className="cookie-banner__button" onClick={acceptCookies}>Accept analytics cookies</button>
+          <button className="cookie-banner__button" onClick={rejectCookies}>Reject analytics cookies</button>
         </div>
       </div>
     </div>
-  )
+  ) || null
+}
+
+function cookieExpiry() {
+  var currentDate = new Date();
+  return currentDate.setFullYear(currentDate.getFullYear() + 1);
+}
+
+function getCookie(name) {
+  var nameEQ = name + '='
+    var cookies = document.cookie.split(';')
+    for (var i = 0, len = cookies.length; i < len; i++) {
+      var cookie = cookies[i]
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1, cookie.length)
+      }
+      if (cookie.indexOf(nameEQ) === 0) {
+        return true
+      }
+    }
+    return false
 }
