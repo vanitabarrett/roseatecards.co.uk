2482422c2c3c8ddfe2541aa93e8215775beaa8f9
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-01-08 16:02:25 +0000

    Valentine's Day update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ export default function Home() {
 
       <h1 className="screenreader-only">Welcome to Roseate Cards</h1>
 
-      <a className="home-page__featured" href="/cards">
+      <a className="home-page__featured" href="/cards/valentines">
         <p className="home-page__featured-text-wrapper">
-          <span className="home-page__featured-text">Send a little love</span><br/><span className="home-page__featured-text home-page__featured-large-text">in the post...</span>
+          <span className="home-page__featured-text">Love is</span><br/><span className="home-page__featured-text home-page__featured-large-text">in the air...</span>
         </p>
       </a>
 
@@ -30,15 +30,15 @@ export default function Home() {
       </div>
 
       <div className="home-page__cta gel-layout">
-        <a className="home-page__cta__wrapper gel-layout__item gel-1/2@m gel-1/3@l" href="/cards">
-          <img className="home-page__cta__image" src="/featured/all-cards.jpg" alt="" />
-          <p className="home-page__cta__text">Shop all cards</p>
+        <a className="home-page__cta__wrapper gel-layout__item gel-1/2@m" href="/cards/valentines">
+          <img className="home-page__cta__image" src="/featured/valentines.jpg" alt="" />
+          <p className="home-page__cta__text">Valentine's Day</p>
         </a>
-        <a className="home-page__cta__wrapper gel-layout__item gel-1/2@m gel-1/3@l" href="/cards/mental-health">
+        <a className="home-page__cta__wrapper gel-layout__item gel-1/2@m" href="/cards/mental-health">
           <img className="home-page__cta__image home-page__cta__image--wellbeing" src="/featured/wellbeing.jpg" alt="" />
           <p className="home-page__cta__text">'Just because' cards</p>
         </a>
-        <a className="home-page__cta__wrapper gel-layout__item gel-1/3@l" href="/cards/birthday">
+        <a className="home-page__cta__wrapper gel-layout__item gel-1/1" href="/cards/birthday">
           <img className="home-page__cta__image home-page__cta__image--christmas" src="/featured/birthday.jpg"  alt="" />
           <p className="home-page__cta__text">Birthday cards</p>
         </a>
