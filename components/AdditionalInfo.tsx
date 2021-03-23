f099e7f82ffc06b9ece5c2276d40a3aa31fd9c65
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-03-23 16:29:50 +0000

    Add wedding sustainability info

--- components/AdditionalInfo.tsx
+++ components/AdditionalInfo.tsx
@@ -24,6 +24,27 @@ export default function AdditionalInfo({categoryId}: Props) {
       </div>
     )
   }
+  if (categoryId === "wedding") {
+    return (
+      <div className="additional-info">
+        <h2 className="additional-info__title">About our wedding stationery...</h2>
+        <div className="gel-layout">
+          <div className="additional-info__section gel-layout__item gel-1/2@s gel-1/3@m">
+            <img className="additional-info__image" src="/svgs/sustainability-eco.svg" alt="" width="70px" />
+            <p className="additional-info__text">All of our wedding stationery is printed on recyclable papers.</p>
+          </div>
+          <div className="additional-info__section gel-layout__item gel-1/2@s gel-1/3@m">
+            <img className="additional-info__image" src="/svgs/sustainable-fsc.svg" alt="" width="70px" />
+            <p className="additional-info__text">Our wedding stationery is made from sustainably-sourced wood from forests certified by the Forest Stewardship Council (FSC) and printed by an ISO14001 certified company.</p>
+          </div>
+          <div className="additional-info__section gel-layout__item gel-1/3@m">
+            <img className="additional-info__image" src="/svgs/sustainable-card.svg" alt="" width="70px" />
+            <p className="additional-info__text">Our wedding stationery is packaged in cardboard boxes using paper tape and compostable cello bags if needed.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return null;
 }
