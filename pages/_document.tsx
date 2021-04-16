66fbab3871476035246685e81b013f4a8a0968a7
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-04-16 20:24:28 +0100

    Add GTM script on each page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,14 +19,12 @@ class MyDocument extends Document {
             ${ getCookie.toString() }
 
             if (getCookie('cookie_preferences') && getCookie('cookie_preferences') === '1') {
-              if (!getCookie('_ga')) {
-                // Load GTM script
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-59QZCF3');
-              }
+              // Load GTM script
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-59QZCF3');
             }
           ` }}>
           </script>
