35ca7bac93cd7e456258abd3e0c1a45477c1ccbe
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-04-16 19:35:16 +0100

    Testing GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,13 @@ class MyDocument extends Document {
 
             if (getCookie('cookie_preferences') && getCookie('cookie_preferences') === '1') {
               if (!getCookie('_ga')) {
+                // Load GTM script
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-59QZCF3');
+
                 // Load gtag script
                 const script = document.createElement('script');
                 script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-MYGOOGLEID');
@@ -88,6 +95,7 @@ class MyDocument extends Document {
           </style>
         </Head>
         <body>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59QZCF3" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
           <Main />
           <NextScript />
         </body>
