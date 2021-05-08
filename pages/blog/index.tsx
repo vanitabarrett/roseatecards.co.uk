c852de72fc505a4428b1baaf55f2145476a0f8b3
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2021-05-08 20:14:28 +0100

    Adjust featured blog placeholder text

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -19,11 +19,10 @@ export default function Blog() {
         <img src="/homepage-feature/just-because.jpg" className="featured-item__image gel-layout__item gel-1/1 gel-1/2@m" alt="" loading="lazy" />
         <div className="featured-item__text-wrapper gel-layout__item gel-1/1 gel-1/2@m">
           <p className="featured-item__headline-wrapper">
-            <span className="featured-item__text">Send a little love</span><br/><span className="featured-item__text featured-item__large-text">in the post...</span>
+            <span className="featured-item__text">date</span><br/><span className="featured-item__text">Blog title</span>
           </p>
-          <p className="featured-item__description">During hard times, messages of love and support can mean a lot to the people we love.</p>
-          <p className="featured-item__description">Our eco-friendly greeting cards and luxury wedding stationery are printed on high quality FSC-approved paper, paired with recycled kraft envelopes and shipped in recyclable cellophane and cardboard.</p>
-          <p className="featured-item__button-wrapper"><a href="/cards/just-because" className="featured-item__button">Shop now</a></p>
+          <p className="featured-item__description">Blog summary.</p>
+          <p className="featured-item__button-wrapper"><a href="/" className="featured-item__button">Continue reading</a></p>
         </div>
       </div>
     </div>
